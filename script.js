46eb36dd0eecd3e51a989244fc45df908c68f5cf
SAULvaRGAS88 <saulvargascosta@hotmail.com>
2022-09-06 19:09:44 -0300

    localStorege implementação

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 // <!-- fazer uma tela, com formulario, onde terá um nome e um telefone usando html, css e js salvando no localStorege -->
-var pessoa = [];
+let pessoa = [];
 function enviar(){
     var nom = document.getElementById('nome').value;
     var num = document.getElementById('cell').value;
 
     pessoa.push({ 'nome': nom, 'numero': num})
-    
+
+    localStorage.setItem('pessoa', JSON.stringify(pessoa));
+    let pessoaString = localStorage.getItem('pessoa');
+    let pessoaObj = JSON.parse(pessoaString);
+    console.log(pessoaObj.nom.value); 
     console.log(pessoa)
 }
+
+
