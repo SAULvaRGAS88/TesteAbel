d7e66b474c5f6bee61a0d062e593c390525859ae
SAULvaRGAS88 <saulvargascosta@hotmail.com>
2022-09-22 22:01:50 -0300

    Add tecnicas de codeClear e refatorando codigo

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-// <!-- fazer uma tela, com formulario, onde terá um nome e um telefone usando html, css e js salvando no localStorege -->
+// <!-- fazer uma tela, com formulario, onde terá um nome e um telefone usando html e js para salvar no localStorege -->
 let pessoa = [];
-function enviar(){
+function enviar() {
     var nom = document.getElementById('nome').value;
     var num = document.getElementById('cell').value;
 
-    pessoa.push({ 'nome': nom, 'numero': num})
+    //foi adicionado uma fução, onde apos o invio dos dados, o input fica limpo, pronto para receber novos dados
+    document.getElementById('nome').value=''; // Limpa o campo
+    document.getElementById('cell').value=''; // Limpa o campo
+
+    pessoa.push({ 'nome': nom, 'numero': num })
 
     localStorage.setItem('pessoa', JSON.stringify(pessoa));
     let pessoaString = localStorage.getItem('pessoa');
     let pessoaObj = JSON.parse(pessoaString);
-    console.log(pessoaObj.nom.value); 
+
+    
+    //.value estava bunado o codigo. sua remoção fez com que o bug fosse resolvido
+    console.log(pessoaObj.nom);
     console.log(pessoa)
 }
-
-
